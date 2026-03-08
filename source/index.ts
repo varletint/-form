835ad6d6d17d50d324373b333cc7196efe5d52f7
varletint <intvarlet@hotmail.com>
2026-03-08 22:36:40 +0100

    source root

---
+++ source/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import responseRoutes from "./routes/responseRoutes";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parses incoming JSON requests
+
+// Routes
+app.use("/api/responses", responseRoutes);
+
+// Health Check Endpoint
+app.get("/health", (req, res) => {
+  res
+    .status(200)
+    .json({ status: "ok", message: "Persistent Forms API is running" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
